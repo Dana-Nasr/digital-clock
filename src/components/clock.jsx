b63efe244efaf7a8b7349996a3445c4eab496380
dananasr2004 <dananasr871@gmail.com>
2024-11-17 21:15:03 +0200

    CREATE: function to set and increment time

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,13 +1,23 @@
-import { useEffect } from "react"
-import  { useState } from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 import "../styles/clock.css";
-function DigitalClock(){
-return (
-    <div className="clock-container">
-        <div className="clock"><span>
-            </span>00:00:00</div>
+function DigitalClock() {
+  const [time, setTime] = useState(new Date()); //current date
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(new Date());
+    }, 1000); //repeat every 1s
+    return () => {
+      clearInterval(intervalID); //free up resources if we unmount the clock comp
+    };
+  }, []); //start only on mount
 
+  return (
+    <div className="clock-container">
+      <div className="clock">
+        <span>00:00:00</span>
+      </div>
     </div>
-)
+  );
 }
 export default DigitalClock;
